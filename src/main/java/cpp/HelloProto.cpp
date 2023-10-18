a4c27775802f2ca14e0a64916de399a30a34e441
zhengzw <zhengzhongwei0@gmail.com>
2023-10-18 16:51:51 +0800

    jin proto demo

--- src/main/java/cpp/HelloProto.cpp
+++ src/main/java/cpp/HelloProto.cpp
@@ -1,17 +1,9 @@
-// ConsoleApplication3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
 #include<stdio.h>
 #include <iostream>
 #include "hello.pb.h"
 #include "proto_HelloProto.h"
 
-
 using namespace std;
-
-typedef unsigned char byte;
-
-jbyte bytes[10000];
 
 JNIEXPORT jbyteArray JNICALL Java_proto_HelloProto_hello(JNIEnv* env, jclass clazz, jbyteArray protoBytes) {
     jsize length = env->GetArrayLength(protoBytes);
@@ -24,9 +16,10 @@ JNIEXPORT jbyteArray JNICALL Java_proto_HelloProto_hello(JNIEnv* env, jclass cla
     env->ReleaseByteArrayElements(protoBytes, byteArrayElements, 0);
 
     int respLength = resp.ByteSizeLong();
-    resp.SerializeToArray(bytes, respLength);
+    vector<jbyte> bytes(respLength);
+    resp.SerializeToArray(bytes.data(), respLength);
     jbyteArray respBytes = env->NewByteArray(respLength);
-    env->SetByteArrayRegion(respBytes, 0, respLength, bytes);
+    env->SetByteArrayRegion(respBytes, 0, respLength, bytes.data());
     return respBytes;
 }
 
